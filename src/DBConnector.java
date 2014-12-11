3cc77101bcdea8898edcedce6926e2a90ede2ce4
Ossi <rostwald@kpmg.com>
2014-12-11 16:58:39 +0100

    Updated DeleteButtons and Functions, changed Layout

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -3,118 +3,189 @@ import java.lang.*;
 import java.sql.*;
 
 public class DBConnector {
-
-    // Variables
-    public String password;
-    public String user;
-    public String connectionString;
-    Connection conn = null;
-    PreparedStatement prepQ = null;
-    ResultSet rsSet = null;
-
-    // Constructor
-    public DBConnector() {
+  
+  // Variables
+  public String password;
+  public String user;
+  public String connectionString;
+  Connection conn = null;
+  PreparedStatement prepQ = null;
+  ResultSet rsSet = null;
+  
+  // Constructor
+  public DBConnector() {
+  }
+  
+  // Constructor with parameters
+  public DBConnector(String xconnectionString, String xuser, String xpassword) {
+    password = xpassword;
+    user = xuser;
+    connectionString = xconnectionString;
+  }
+  
+  // Set and Get Methods
+  public void setUser(String xuser){
+    this.user = xuser;
+  }
+  public void setPassword(String xpassword){
+    password = xpassword;
+  }
+  public void setConnectionString(String xconnectionString) {
+    connectionString = xconnectionString;
+  }
+  
+  public String getPassword() {
+    return password;
+  }
+  public String getUser() {
+    return user;
+  }
+  public String getConnectionString() {
+    return connectionString;
+  }
+  
+  // Connect to Database
+  public Connection connectToMSSQL() {
+    Connection sqlDBCon = null;
+    try {
+      Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+      sqlDBCon = DriverManager.getConnection(connectionString, user, password);
+      System.out.println("connected to " + connectionString);
     }
-
-    // Constructor with parameters
-    public DBConnector(String xconnectionString, String xuser, String xpassword) {
-        password = xpassword;
-        user = xuser;
-        connectionString = xconnectionString;
+    catch (Exception e) {
+      e.printStackTrace();
     }
-
-    // Set and Get Methods
-    public void setUser(String xuser){
-        this.user = xuser;
+    return sqlDBCon;
+  }
+  
+  /// Database Querys ////////////////////////////////////////////////////////////////////////////////////////////////////
+  public void getEmpByID(Connection conn, double id) {
+    // Setting Query
+    String query = "SELECT * FROM T_EMP WHERE EMP_ID = ?";
+    try {
+      prepQ = conn.prepareStatement(query);
+      // Replace "?" in Query
+      prepQ.setDouble(1, id);
+      // Save Result in rsSet
+      rsSet = prepQ.executeQuery();
+      // If result is not empty
+      if (rsSet != null && rsSet.next()) {
+        System.out.println(rsSet.getString("EMP_ID") + " | " + rsSet.getString("Name") + " | " + rsSet.getString("PreName"));
+        prepQ.close();
+      }
+      else {
+        System.out.println("EMP ID does not exist");
+      }
     }
-    public void setPassword(String xpassword){
-        password = xpassword;
+    catch (SQLException ex) {
+      ex.printStackTrace();
     }
-    public void setConnectionString(String xconnectionString) {
-        connectionString = xconnectionString;
+  }
+  
+  public void getItemByID(Connection conn, double id) {
+    // Setting Query
+    String query = "SELECT * FROM T_INV WHERE INV_ID = ?";
+    try {
+      prepQ = conn.prepareStatement(query);
+      // Replace "?" in Query
+      prepQ.setDouble(1, id);
+      // Save Result in rsSet
+      rsSet = prepQ.executeQuery();
+      // If result is not empty
+      if (rsSet != null && rsSet.next()) {
+        System.out.println(rsSet.getString("INV_ID") + " | " + rsSet.getString("Name") + " | " + rsSet.getString("Type"));
+        prepQ.close();
+      }
+      else {
+        System.out.println("INV ID does not exist");
+      }
     }
-
-    public String getPassword() {
-        return password;
+    catch (SQLException ex) {
+      ex.printStackTrace();
     }
-    public String getUser() {
-        return user;
+    catch (NullPointerException nex) {
+      nex.printStackTrace();
     }
-    public String getConnectionString() {
-        return connectionString;
+    
+  }
+  
+  public void setItemByID(Connection conn, double id) {
+    // Setting Query
+    String query = "INSERT INTO T_INV (Inv_ID) VALUES (?)";
+    try {
+      prepQ = conn.prepareStatement(query);
+      // Replace "?" in Query
+      prepQ.setDouble(1, id);
+      prepQ.execute();
+      System.out.println("Die INV ID wurde in die Tabelle eingetragen!");
+      prepQ.close();
     }
-
-    // Connect to Database
-    public Connection connectToMSSQL() {
-        Connection sqlDBCon = null;
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            sqlDBCon = DriverManager.getConnection(connectionString, user, password);
-            System.out.println("connected to " + connectionString);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        return sqlDBCon;
+    
+    catch (SQLException ex) {
+      ex.printStackTrace();
     }
-
-/// Database Querys ////////////////////////////////////////////////////////////////////////////////////////////////////
-    public void getEmpByID(Connection conn, double id) {
-        // Setting Query
-        String query = "SELECT * FROM T_EMP WHERE EMP_ID = ?";
-        try {
-            prepQ = conn.prepareStatement(query);
-            // Replace "?" in Query
-            prepQ.setDouble(1, id);
-            // Save Result in rsSet
-            rsSet = prepQ.executeQuery();
-            // If result is not empty
-            if (rsSet != null && rsSet.next()) {
-                System.out.println(rsSet.getString("EMP_ID") + " | " + rsSet.getString("Name") + " | " + rsSet.getString("PreName"));
-                prepQ.close();
-            }
-            else {
-                System.out.println("EMP ID does not exist");
-            }
-        }
-        catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+    catch (NullPointerException nex) {
+      nex.printStackTrace();
     }
-
-    public void getItemByID(Connection conn, double id) {
-        // Setting Query
-        String query = "SELECT * FROM T_INV WHERE INV_ID = ?";
-        try {
-            prepQ = conn.prepareStatement(query);
-            // Replace "?" in Query
-            prepQ.setDouble(1, id);
-            // Save Result in rsSet
-            rsSet = prepQ.executeQuery();
-            // If result is not empty
-            if (rsSet != null && rsSet.next()) {
-                System.out.println(rsSet.getString("INV_ID") + " | " + rsSet.getString("Name") + " | " + rsSet.getString("Type"));
-                prepQ.close();
-            }
-            else {
-                System.out.println("INV ID does not exist");
-            }
-        }
-        catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-
+    
+  }
+  
+  public void setEmpByID(Connection conn, double id) {
+    // Setting Query
+    String query = "INSERT INTO T_EMP (EMP_ID) VALUES (?)";
+    try {
+      prepQ = conn.prepareStatement(query);
+      // Replace "?" in Query
+      prepQ.setDouble(1, id);
+      prepQ.execute();
+        System.out.println("Die EMP ID wurde in die Tabelle eingetragen!");
+        prepQ.close();
     }
+    catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+    catch (NullPointerException nex) {
+      nex.printStackTrace();
+    }
+    
+  }
 
-    public boolean createItem() {return false;}
+  public void deleteEmpByID(Connection conn, double id) {
+    // Setting Query
+    String query = "DELETE FROM T_EMP WHERE EMP_ID = (?)";
+    try {
+      prepQ = conn.prepareStatement(query);
+      prepQ.setDouble(1, id);
+      prepQ.execute();
+      System.out.println("EMP " + id + " has been deleted.");
+      prepQ.close();
+    }
+    catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+  }
 
-    public boolean deleteItem() {return true;}
+  public void deleteInvByID(Connection conn, double id) {
+    // Setting Query
+    String query = "DELETE FROM T_INV WHERE INV_ID = (?)";
+    try {
+      prepQ = conn.prepareStatement(query);
+      prepQ.setDouble(1, id);
+      prepQ.execute();
+      System.out.println("Item with ID " + id + " has been deleted.");
+      prepQ.close();
+    }
+    catch (SQLException ex) {
+      ex.printStackTrace();
+    }
+  }
 
-    public boolean setEmp() {return true;}
+  
 
-
-
+  
+  
+  
 }
-
-
-
+  
+  
+  
--- src/userGUI.java
+++ src/userGUI.java
@@ -1,4 +1,4 @@
-import sun.util.resources.TimeZoneNames_zh_CN;
+// import sun.util.resources.TimeZoneNames_zh_CN;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -10,116 +10,212 @@ import java.sql.Connection;
  */
 
 public class userGUI extends JFrame {
-    private JButton bt_searchInvID;
-    private JButton bt_searchEmpID;
-    private JLabel lb_EmpID;
-    private JLabel lb_InvID;
-    private JTextField tf_EmpID;
-    private JTextField tf_InvID;
+  private JButton bt_searchInvID;
+  private JButton bt_searchEmpID;
+  private JButton bt_createInvID;
+  private JButton bt_createEmpID;
+  private JButton bt_deleteEmpID;
+  private JButton bt_deleteInvID;
+  private JLabel lb_EmpID;
+  private JLabel lb_InvID;
+  private JTextField tf_EmpID;
+  private JTextField tf_InvID;
 
-    // Creating Object of DBConnector-class, providing connectionString and SQL user credentials
-    DBConnector obj_db = new DBConnector("jdbc:sqlserver://DEFR2APP31\\BO_TEST;databaseName=Test", "asproject", "Blumen99");
-    // Save connection as an object to "conn"
-    Connection conn = obj_db.connectToMSSQL();
+  // Creating Object of DBConnector-class, providing connectionString and SQL user credentials
+  // DBConnector obj_db = new DBConnector("jdbc:sqlserver://localhost;databaseName=master", "sa", "Blumen99");
+  DBConnector obj_db = new DBConnector("jdbc:sqlserver://DEFR2APP31\\BO_TEST;databaseName=Test", "asproject", "Blumen99");
+  // Save connection as an object to "conn"
+  Connection conn = obj_db.connectToMSSQL();
 
-    public userGUI() {
+  public userGUI() {
 
-        // Name of Window (top left)
-        super("InventO - Alpha");
+    // Name of Window (top left)
+    super("InventO - Alpha");
 
-        // Close on [X} Press
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    // Close on [X} Press
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        // Denying Layout Manager
-        this.getContentPane().setLayout(null);
+    // Denying Layout Manager
+    this.getContentPane().setLayout(null);
 
-        // Creating Elements
-        lb_EmpID = new JLabel("Employee ID");
-        lb_InvID = new JLabel("Inventory ID");
-        bt_searchInvID = new JButton("Search for Inventory ID");
-        bt_searchEmpID = new JButton ("Search for Employee ID");
-        tf_EmpID = new JTextField("");
-        tf_InvID = new JTextField("");
+    // Creating Elements
+    lb_EmpID = new JLabel("Employee ID");
+    lb_InvID = new JLabel("Inventory ID");
+    bt_searchInvID = new JButton("Search for Inventory ID");
+    bt_searchEmpID = new JButton("Search for Employee ID");
+    bt_createInvID = new JButton("Create Inventory Item by ID");
+    bt_createEmpID = new JButton("Create Employee by ID");
+    bt_deleteEmpID = new JButton("Delete Employee");
+    bt_deleteInvID = new JButton("Delete Item by ID");
+    tf_EmpID = new JTextField("");
+    tf_InvID = new JTextField("");
 
-        // Setting Bounds for Inventory ID
-        bt_searchInvID.setBounds(220,40,180,25);
-        lb_InvID.setBounds(10,10,100,25);
-        tf_InvID.setBounds(10,40,200,25);
+    // Setting Bounds for Inventory ID
+    bt_searchInvID.setBounds(220, 40, 180, 25);
+    bt_createInvID.setBounds(220, 70, 180, 25);
+    lb_InvID.setBounds(10, 10, 100, 25);
+    tf_InvID.setBounds(10, 40, 200, 25);
 
-        // Setting Bounds for Emp ID
-        bt_searchEmpID.setBounds(220,120,180,25);
-        lb_EmpID.setBounds(10,90,100,25);
-        tf_EmpID.setBounds(10,120,200,25);
+    // Setting Bounds for Emp ID
+    bt_searchEmpID.setBounds(220, 150, 180, 25);
+    bt_createEmpID.setBounds(10, 190, 200, 25);
+    bt_deleteEmpID.setBounds(220, 190, 180, 25);
+    lb_EmpID.setBounds(10, 120, 100, 25);
+    tf_EmpID.setBounds(10, 150, 200, 25);
 
 
-        // Adding Elements to Window
-        this.getContentPane().add(lb_EmpID);
-        this.getContentPane().add(lb_InvID);
-        this.getContentPane().add(bt_searchInvID);
-        this.getContentPane().add(bt_searchEmpID);
-        this.getContentPane().add(tf_InvID);
-        this.getContentPane().add(tf_EmpID);
-        this.pack();
+    // Adding Elements to Window
+    this.getContentPane().add(lb_EmpID);
+    this.getContentPane().add(lb_InvID);
+    this.getContentPane().add(bt_searchInvID);
+    this.getContentPane().add(bt_createInvID);
+    this.getContentPane().add(bt_searchEmpID);
+    this.getContentPane().add(bt_createEmpID);
+    this.getContentPane().add(tf_InvID);
+    this.getContentPane().add(tf_EmpID);
+    this.getContentPane().add(bt_deleteInvID);
+    this.getContentPane().add(bt_deleteEmpID);
+    this.pack();
 
-        // Moving GUI to Center
-        this.setLocationRelativeTo(null);
+    // Moving GUI to Center
+    this.setLocationRelativeTo(null);
 
-        // ActionListener to bt_searchEmpID
-        bt_searchEmpID.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                searchEmpID_clicked();
-            }
-        });
+    // ActionListener to bt_searchEmpID
+    bt_searchEmpID.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        searchEmpID_clicked();
+      }
+    });
 
-        // ActionListener to bt_searchInvID
-        bt_searchInvID.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                searchInvID_clicked();
-            }
-        });
-    }  // End of UserGUI Constructor
+    // ActionListener to bt_searchInvID
+    bt_searchInvID.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        searchInvID_clicked();
+      }
+    });
+    // ActionListener to bt_createEmpID
+    bt_createEmpID.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        createEmpID_clicked();
+      }
+    });
 
-/// Action performed, when bt_searchEmpID is clicked
-    public void searchEmpID_clicked() {
-        double empID;
-        try {
-            empID = Double.parseDouble(tf_EmpID.getText());
-        } catch (NumberFormatException e) {
-            empID = 0;
-        }
-        if (empID == 0) {
-            System.out.println("Please enter a valid Employee ID");
-        }
-        else {
-            obj_db.getEmpByID(conn, empID);
-        }
+    // ActionListener to bt_createInvID
+    bt_createInvID.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        createInvID_clicked();
+      }
+    });
+
+    // ActionListener to bt_deleteEmp
+    bt_deleteEmpID.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        deleteEmpID_clicked();
+      }
+    });
+
+    // ActionListener to bt_deleteInv
+    bt_deleteInvID.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        deleteInvID_clicked();
+      }
+    });
+
+  }  // End of UserGUI Constructor
+
+  /// Action performed, when bt_searchEmpID is clicked
+  public void searchEmpID_clicked() {
+    double empID;
+    try {
+      empID = Double.parseDouble(tf_EmpID.getText());
+    } catch (NumberFormatException e) {
+      empID = 0;
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/// Action performed, when bt_searchInvID is clicked
-    public void searchInvID_clicked() {
-        double invID;
-        try {
-            invID = Double.parseDouble(tf_InvID.getText());
-        } catch (NumberFormatException e) {
-            invID = 0;
-        }
-        if (invID == 0) {
-            System.out.println("Please enter a valid Inventory ID");
-        } else {
-            obj_db.getItemByID(conn, invID);
-        }
+    if (empID == 0) {
+      System.out.println("Please enter a valid Employee ID");
+    } else {
+      obj_db.getEmpByID(conn, empID);
     }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  }
 
-    public static void main(String[] args) {
-        userGUI mainGUI = new userGUI();
-        mainGUI.setBounds(100,100,520,400);
-        mainGUI.show();
-
+  /// Action performed, when bt_searchInvID is clicked
+  public void searchInvID_clicked() {
+    double invID;
+    try {
+      invID = Double.parseDouble(tf_InvID.getText());
+    } catch (NumberFormatException e) {
+      invID = 0;
     }
+    if (invID == 0) {
+      System.out.println("Please enter a valid Inventory ID");
+    } else {
+      obj_db.getItemByID(conn, invID);
+    }
+  }
 
+  /// Action performed, when bt_createInvID is clicked
+  public void createInvID_clicked() {
+    double invID;
+    try {
+      invID = Double.parseDouble(tf_InvID.getText());
+    } catch (NumberFormatException e) {
+      invID = 0;
+    }
+    if (invID == 0) {
+      System.out.println("Please enter a valid Inventory ID");
+    } else {
+      obj_db.setItemByID(conn, invID);
+    }
+  }
 
+  /// Action performed, when bt_createEmpID is clicked
+  public void createEmpID_clicked() {
+    double empID;
+    try {
+      empID = Double.parseDouble(tf_EmpID.getText());
+    } catch (NumberFormatException e) {
+      empID = 0;
+    }
+    if (empID == 0) {
+      System.out.println("Please enter a valid Employee ID");
+    } else {
+      obj_db.setEmpByID(conn, empID);
+    }
+  }
+
+  /// Action performed, when bt_deleteEmpID is clicked
+  public void deleteEmpID_clicked() {
+    double empID;
+    try {
+      empID = Double.parseDouble(tf_EmpID.getText());
+    } catch (NumberFormatException e) {
+      empID = 0;
+    }
+    if (empID == 0) {
+      System.out.println("Please enter a valid Employee ID");
+    } else {
+      obj_db.deleteEmpByID(conn, empID);
+    }
+  }
+
+  /// Action performed, when bt_deleteInvID is clicked
+  public void deleteInvID_clicked() {
+    double empID;
+    try {
+      empID = Double.parseDouble(tf_EmpID.getText());
+    } catch (NumberFormatException e) {
+      empID = 0;
+    }
+    if (empID == 0) {
+      System.out.println("Please enter a valid Employee ID");
+    } else {
+      obj_db.deleteInvByID(conn, empID);
+    }
+  }
 }
